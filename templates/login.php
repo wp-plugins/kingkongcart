51cb7408902077aebf0bc6fd6d77f07d73bbd24c
ithemeso <ithemeso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 04:16:08 +0000

    0.2.2
    git-svn-id: https://plugins.svn.wordpress.org/kingkongcart/tags/0.2.2@949321 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/login.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Login Page
+*/
+
+	if(!is_user_logged_in()){
+?>
+<?php get_header(); ?>
+
+<div class="entry-content" style="text-align:center">
+
+<h2><?php //the_title(); ?></h2>
+
+<form name="loginform" id="loginform" action="<?php echo get_option('home'); ?>/wp-login.php" method="post">
+	<p>
+		<label>아이디<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" tabindex="10" /></label>
+	</p>
+	<p>
+
+		<label>비밀번호<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" tabindex="20" /></label>
+	</p>
+	<p class="forgetmenot"><label><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="90" /> 기억하기</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Log In" tabindex="100" />
+		<input type="hidden" name="redirect_to" value="<?php echo get_option('home'); ?>/wp-admin/" />
+
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+<a href="<?php echo get_option('home'); ?>/wp-login.php?action=lostpassword" title="Password Lost and Found">비밀번호 찾기</a>
+</p>
+
+</div>
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
+
+<?php
+	}
+?>
