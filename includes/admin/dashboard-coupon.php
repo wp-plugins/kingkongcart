867636cc9e8705fa3fc40581484d880694613351
ithemeso <ithemeso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-22 09:18:32 +0000

    upload 0.4.7
    git-svn-id: https://plugins.svn.wordpress.org/kingkongcart/tags/0.4.7@970497 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin/dashboard-coupon.php
@@ -0,0 +1,19 @@
+<ul>
+	<li><span class="dashboard-content-title">쿠폰설정</span></li>
+	<li>
+		<span class="dashboard-content-subtitle">
+		쿠폰 발행과 설정을 하실 수 있습니다.
+		</span>
+	</li>
+</ul>
+<table>
+	<tr>
+		<th>번호</th>
+		<th>종류</th>
+		<th>쿠폰명</th>
+		<th>할인금액</th>
+		<th>발급수</th>
+		<th>기간</th>
+	</tr>
+</table>
+		
